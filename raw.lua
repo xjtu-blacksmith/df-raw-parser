351a53e5ca5eabf828373a84700691a128d00aa9
xjtu-blacksmith <xjbs@protonmail.com>
2022-01-03 22:07:39 +0800

    add: raw module for mediawiki

---
+++ raw.lua
@@ -0,0 +1,100 @@
+local p = {}
+
+p.getRaw = function (db, name, token1, token2)
+    local data = mw.loadData('Module:raw/' .. db)
+    if not data[name] then return nil end
+    local token_db = data[name].tab
+    if not token1 then token1 = 'NAME' end  -- default token
+    if not token2 then
+    	if token_db[token1] then
+        	return token_db[token1] -- first level
+        else
+        	return nil
+        end
+    else
+        if token_db[token1] and token_db[token1].tab[token2] then
+        	return token_db[token1].tab[token2] -- second level
+        else
+        	return nil
+        end
+    end
+end
+
+p.dfRaw = function ( frame )
+    local args = frame.args
+	if frame == mw.getCurrentFrame() then
+		args = frame:getParent().args
+	end
+    local db = args[1]
+    local name = args[2]
+    local token1 = args[3]
+    local token2 = args[4]
+    return p.getRaw( db, name, token1, token2 )
+end
+
+p.tagValue = function (frame)
+    local args = frame.args
+	if frame == mw.getCurrentFrame() then
+		args = frame:getParent().args
+	end
+    local db = args[1]
+    local name = args[2]
+    local token1 = args[3]
+    local token2 = args[4]
+    local token = p.getRaw (db, name, token1, token2)
+    if token and token.val then
+        return token.val
+    else
+        return ""
+    end
+end
+
+p.tagEntry = function( frame )
+    local args = frame.args
+	if frame == mw.getCurrentFrame() then
+		args = frame:getParent().args
+	end
+    local db = args[1]
+    local name = args[2]
+    local token_name = args[3]
+    local index = args[4]
+    local token = p.getRaw (db, name, token_name)
+    if token and token.val then
+        return token.val[index]
+    else
+        return ""
+    end
+end
+
+p.tag = function( frame )
+    local args = frame.args
+	if frame == mw.getCurrentFrame() then
+		args = frame:getParent().args
+	end
+    local db = args[1]
+    local name = args[2]
+    local token1 = args[3]
+    local token2 = args[4]
+    local token = p.getRaw (db, name, token1, token2)
+    if token and token.val then  -- tag exists
+        return "true"  -- representing true
+    else
+        return ""  -- empty string means false for wikitext
+    end
+end
+
+p.getStateDescription = function ( frame )
+    local args = frame.args
+	if frame == mw.getCurrentFrame() then
+		args = frame:getParent().args
+	end
+    local token = args[1]
+    local entry = args[2]
+    local state = args[3]
+    if entry == 'NAME' then
+        return token.val['STATE_NAME_ADJ/' .. state].val
+    end
+    return ""  -- default: empty string
+end
+
+return p
