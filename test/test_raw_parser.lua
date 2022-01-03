0e8f1056b9fb9df8e1d991793e4c96c2419d12d7
xjtu-blacksmith <xjbs@protonmail.com>
2022-01-03 22:09:54 +0800

    update: test script works under Windows

--- test/test_raw_parser.lua
+++ test/test_raw_parser.lua
@@ -1,4 +1,24 @@
+package.path = './?.lua'
 p = require('raw_parser')
-raw = p.read_file('raw/plant_standard.txt')
-data = p.parse(raw)
-print(p.dump(data))
+data = {}
+files = {
+    'plant_crops.txt',
+    'plant_garden.txt',
+    'plant_grasses.txt',
+    'plant_new_trees.txt',
+    'plant_standard.txt'
+}
+for _, file in ipairs(files) do
+    -- read raw file
+    raw = p.read_file('raw/' .. file)
+    data = p.parse(raw, data)
+    print(file .. ' processed!')
+end
+
+-- write out data
+f = io.output('out/plant.lua')
+f:write('return ' .. p.dump(data))
+-- f:write('return ' .. p.dump(data, "", true))
+f:close()
+
+-- test data info
